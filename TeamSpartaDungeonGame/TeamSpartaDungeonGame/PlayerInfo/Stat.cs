727ff9034e72fdea2485b3a067fa12c230bcff56
CHICHI <smartb12@naver.com>
2024-05-03 22:18:55 +0900

    충돌 복구중
    Stat.cs 복구중

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,51 @@ namespace TeamSpartaDungeonGame.PlayerInfo
 {
     public class Stat
     {
-        public int lv;  // 레  벨
-        public int atk; // 공격력
-        public int def; // 방어력
-        public int hp;  // 체  력
-        public int mp;  // 마  나
+        Player player;
 
-        public int crit; // 크리티컬 확률
-        public int critd;// 크리티컬 데미지
-        public int dodge;// 회피 확률
+        string Name { get; set; }
+        public int Lv { get; set; }  // 레  벨
+        public int Atk { get; set; } // 공격력
+        public int Def { get; set; } // 방어력
+        public int Hp { get; set; }  // 체  력
+        public int Mp { get; set; }  // 마  나
+        public int Gold { get; set; }
+        public int Crit { get; set; } // 크리티컬 확률
+        public int Critd { get; set; }// 크리티컬 데미지
+        public int Dodge { get; set; }// 회피 확률
+
+        public Stat()
+        {
+            Name = "";
+            Lv = 1;
+            Atk = 10;
+            Def = 10;
+            Hp = 100;
+            Mp = 50;
+            Crit = 10;
+            Critd = 160;
+            Dodge = 10;
+            Gold = 2000;
+        }
+
+        public void PlayerStatus()
+        {
+            Console.Clear();
+
+            Console.WriteLine("     [스탯 창]     \n");
+            Console.WriteLine($"이  름         : {Name}");
+            Console.WriteLine($"LEVEL          : {Lv}");
+            Console.WriteLine($"공격력         : {Atk}");
+            Console.WriteLine($"방어력         : {Def}");
+            Console.WriteLine($"체  력         : {Hp}");
+            Console.WriteLine($"마  나         : {Mp}");
+            Console.WriteLine($"크리티컬 확률   : {Crit}");
+            Console.WriteLine($"크리티컬 데미지 : {Critd}");
+            Console.WriteLine($"회피 확률 : {Dodge}\n");
+            Console.WriteLine($"보유 골드 : {Gold}\n");
+        }
+
+
     }
 }
+
