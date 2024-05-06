58b1cf74446d3e2bc3331cd678e0278dbc73ebe0
youminkim <kym1787@naver.com>
2024-05-06 15:39:32 +0900

    상점 인벤토리 수정

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/Content/Shop.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/Content/Shop.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TeamSpartaDungeonGame.Interface;
 using TeamSpartaDungeonGame.ItemInfo;
 using TeamSpartaDungeonGame.PlayerInfo;
+using TeamSpartaDungeonGame.Utility;
 
 namespace TeamSpartaDungeonGame.Content
 {
@@ -16,6 +17,7 @@ namespace TeamSpartaDungeonGame.Content
         private bool IsExit;
         private Player player;
         private List<Item> storeInventory;
+        private List<Item> inventory;
 
         
 
@@ -62,6 +64,138 @@ namespace TeamSpartaDungeonGame.Content
                 Update();
             }
 
+        }
+
+        private int max = Pagecount;
+        private int min = 0;
+        const int Pagecount = 9;
+        private void StoreMenu()
+        {
+            Console.Clear();
+
+            ConsoleUtility.ShowTitle("■ 상점 ■");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine("");
+            Console.WriteLine("[보유 골드]");
+            ConsoleUtility.PrintTextHighlights("", player.Gold.ToString(), "G");
+            Console.WriteLine("");
+            Console.WriteLine("[아이템 목록]");
+            for (int i = min; i < max; i++)
+            {
+                storeInventory[i].PrintStoreItemStatDesciption();
+            }
+            Console.WriteLine("");
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 다음 페이지");
+            Console.WriteLine("3. 이전 페이지");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("");
+            switch (ConsoleUtility.PromptMenuChoice(0, 3))
+            {
+                case 0:
+                    MainMenu();
+                    break;
+                case 1:
+                    PurchaseMenu();
+                    break;
+                case 2:
+                    NextPage();
+                    StoreMenu();
+                    break;
+                case 3:
+                    PreviousPage();
+                    StoreMenu();
+                    break;
+            }
+
+        }
+        public void NextPage()
+        {
+            if (max == storeInventory.Count)
+            {
+                return;
+            }//void 에서 return은 메서드 탈출을 의미한다.
+
+            min = max;
+            max += Pagecount;
+
+            if (max > storeInventory.Count)
+            {
+                max = storeInventory.Count;
+            }
+        }
+
+        public void PreviousPage()
+        {
+            if (min == 0)
+            {
+                return;
+            }
+
+            max = min;
+            min -= Pagecount;
+
+            if (min < 0)
+            {
+                min = 0;
+            }
+        }
+        private void PurchaseMenu(string? prompt = null)
+        {
+            if (prompt != null)
+            {
+                //1초간 메시지를 띄운 다음에 다시 진행
+                Console.Clear();
+                ConsoleUtility.ShowTitle(prompt);
+                Thread.Sleep(1000);//몇 밀리초동안 멈출것인가?
+            }
+
+            Console.Clear();
+
+            ConsoleUtility.ShowTitle("■ 상점 - 구매하기■");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine("");
+            Console.WriteLine("[보유 골드]");
+            ConsoleUtility.PrintTextHighlights("", player.Gold.ToString(), "G");
+            Console.WriteLine("");
+            Console.WriteLine("[아이템 목록]");
+            for (int i = 0; i < storeInventory.Count; i++)
+            {
+                storeInventory[i].PrintStoreItemStatDesciption(true, i + 1);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("");
+
+            int keyInput = ConsoleUtility.PromptMenuChoice(0, storeInventory.Count);
+
+            switch (keyInput)
+            {
+                case 0:
+                    StoreMenu();
+                    break;
+                default:
+                    //1 : 이미 구매한 경우
+                    if (storeInventory[keyInput - 1].IsPurchased)
+                    {
+                        PurchaseMenu("이미 구매한 아이템입니다.");
+                    }
+                    //2 : 돈이 충분해서 살 수 있는 경우
+                    else if (player.Gold >= storeInventory[keyInput - 1].Price)
+                    {
+                        player.Gold -= storeInventory[keyInput - 1].Price;
+                        storeInventory[keyInput - 1].Purchased();
+                        inventory.Add(storeInventory[keyInput - 1]);
+                        PurchaseMenu();
+                    }
+                    //3 : 돈이 모자라는 경우
+                    else
+                    {
+                        PurchaseMenu("Gold가 부족합니다.");
+                    }
+                    break;
+
+            }
         }
 =======
 >>>>>>> Stashed changes
--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/ItemInfo/Inventory.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/ItemInfo/Inventory.cs
@@ -10,8 +10,12 @@ namespace TeamSpartaDungeonGame.ItemInfo
 {
     internal class Inventory
     {
+        private int max = Pagecount;
+        private int min = 0;
+        const int Pagecount = 9;
+
         private List<Item> inventory;
-        GameManager gameManager = new GameManager(); 
+        GameManager gameManager = new GameManager();
 
         private void InventoryMenu()
         {
@@ -22,25 +26,34 @@ namespace TeamSpartaDungeonGame.ItemInfo
             Console.WriteLine("");
             Console.WriteLine("[아이템 목록]");
 
-            for (int i = 0; i < inventory.Count; i++)
+            for (int i = min; i < max; i++)
             {
                 inventory[i].PrintItemStatDesciption();
             }
 
             Console.WriteLine("");
-            Console.WriteLine("0. 나가기");
             Console.WriteLine("1. 장착관리");
+            Console.WriteLine("2. 다음 페이지");
+            Console.WriteLine("3. 이전 페이지");
+            Console.WriteLine("0. 나가기");
             Console.WriteLine("");
 
-            switch (ConsoleUtility.PromptMenuChoice(0, 1))
+            switch (ConsoleUtility.PromptMenuChoice(0, 3))
             {
                 case 0:
-                    gameManager.MainMenu();
+                    MainMenu();
                     break;
                 case 1:
                     EquipMenu();
                     break;
-
+                case 2:
+                    InvenNextPage();
+                    InventoryMenu();
+                    break;
+                case 3:
+                    InvenPreviousPage();
+                    InventoryMenu();
+                    break;
             }
         }
 
@@ -71,6 +84,38 @@ namespace TeamSpartaDungeonGame.ItemInfo
                     inventory[KeyInput - 1].ToggleEquipStatus();
                     EquipMenu();
                     break;
+            }
+        }
+
+        public void InvenNextPage()
+        {
+            if (max == inventory.Count)
+            {
+                return;
+            }//void 에서 return은 메서드 탈출을 의미한다.
+
+            min = max;
+            max += Pagecount;
+
+            if (max > inventory.Count)
+            {
+                max = inventory.Count;
+            }
+        }
+
+        public void InvenPreviousPage()
+        {
+            if (min == 0)
+            {
+                return;
+            }
+
+            max = min;
+            min -= Pagecount;
+
+            if (min < 0)
+            {
+                min = 0;
             }
         }
     }
