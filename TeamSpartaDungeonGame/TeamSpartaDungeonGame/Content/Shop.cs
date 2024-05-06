d50134e1b278bdd785401a78d075cca3fed1ce8d
youminkim <kym1787@naver.com>
2024-05-06 17:37:53 +0900

    샵 수정

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/Content/Shop.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/Content/Shop.cs
@@ -18,8 +18,10 @@ namespace TeamSpartaDungeonGame.Content
         private Player player;
         private List<Item> storeInventory;
         private List<Item> inventory;
+        private int max = Pagecount;
+        private int min = 0;
+        const int Pagecount = 9;
 
-        
 
         public Shop(Player player) 
         {
@@ -41,35 +43,29 @@ namespace TeamSpartaDungeonGame.Content
             storeInventory.Add(new Item("샤넬 코트", "명품 옷", ItemType.ARMOR, 0, 35, 99, 4000, 0, 0));
             storeInventory.Add(new Item("샤넬 No.5", "명품 향수", ItemType.ACCESSORY, 0, 0, 0, 3000, 9, 9));
         }
-        public void Initalize(Player value) 
+
+        public void StoreMenuUpdate()
         {
-
-        }
-
-        public void Update()
-        {
-
+            switch (ConsoleUtility.PromptMenuChoice(0, 3))
+            {
+                case 0:
+                    //MainMenu();
+                    break;
+                case 1:
+                    PurchaseMenuLoop();
+                    break;
+                case 2:
+                    NextPage();
+                    StoreMenuRender();
+                    break;
+                case 3:
+                    PreviousPage();
+                    StoreMenuRender();
+                    break;
+            }
         }
         
-        public void Render()
-        {
-
-        }
-
-        public void Loop()
-        {
-            while(!IsExit)
-            {
-                Render();
-                Update();
-            }
-
-        }
-
-        private int max = Pagecount;
-        private int min = 0;
-        const int Pagecount = 9;
-        private void StoreMenu()
+        public void StoreMenuRender()
         {
             Console.Clear();
 
@@ -90,25 +86,89 @@ namespace TeamSpartaDungeonGame.Content
             Console.WriteLine("3. 이전 페이지");
             Console.WriteLine("0. 나가기");
             Console.WriteLine("");
-            switch (ConsoleUtility.PromptMenuChoice(0, 3))
+        }
+
+        public void StoreMenuLoop()
+        {
+            while(!IsExit)
             {
-                case 0:
-                    MainMenu();
-                    break;
-                case 1:
-                    PurchaseMenu();
-                    break;
-                case 2:
-                    NextPage();
-                    StoreMenu();
-                    break;
-                case 3:
-                    PreviousPage();
-                    StoreMenu();
-                    break;
+                StoreMenuRender();
+                StoreMenuUpdate();
             }
 
         }
+
+        public void PurchaseMenuUpdate()
+        {
+            int keyInput = ConsoleUtility.PromptMenuChoice(0, storeInventory.Count);
+
+            switch (keyInput)
+            {
+                case 0:
+                    StoreMenuLoop();
+                    break;
+                default:
+                    //1 : 이미 구매한 경우
+                    if (storeInventory[keyInput - 1].IsPurchased)
+                    {
+                        PurchaseMenuRender("이미 구매한 아이템입니다.");
+                    }
+                    //2 : 돈이 충분해서 살 수 있는 경우
+                    else if (player.Gold >= storeInventory[keyInput - 1].Price)
+                    {
+                        player.Gold -= storeInventory[keyInput - 1].Price;
+                        storeInventory[keyInput - 1].Purchased();
+                        inventory.Add(storeInventory[keyInput - 1]);
+                        PurchaseMenuLoop();
+                    }
+                    //3 : 돈이 모자라는 경우
+                    else
+                    {
+                        PurchaseMenuRender("Gold가 부족합니다.");
+                    }
+                    break;
+
+            }
+        }
+
+        public void PurchaseMenuRender(string? prompt = null)
+        {
+            if (prompt != null)
+            {
+                //1초간 메시지를 띄운 다음에 다시 진행
+                Console.Clear();
+                ConsoleUtility.ShowTitle(prompt);
+                Thread.Sleep(1000);//몇 밀리초동안 멈출것인가?
+            }
+
+            Console.Clear();
+
+            ConsoleUtility.ShowTitle("■ 상점 - 구매하기■");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine("");
+            Console.WriteLine("[보유 골드]");
+            ConsoleUtility.PrintTextHighlights("", player.Gold.ToString(), "G");
+            Console.WriteLine("");
+            Console.WriteLine("[아이템 목록]");
+            for (int i = 0; i < storeInventory.Count; i++)
+            {
+                storeInventory[i].PrintStoreItemStatDesciption(true, i + 1);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("");
+        }
+
+        public void PurchaseMenuLoop()
+        {
+            while (!IsExit)
+            {
+                PurchaseMenuRender();
+                PurchaseMenuUpdate();
+            }
+        }
+
+
         public void NextPage()
         {
             if (max == storeInventory.Count)
@@ -140,63 +200,7 @@ namespace TeamSpartaDungeonGame.Content
                 min = 0;
             }
         }
-        private void PurchaseMenu(string? prompt = null)
-        {
-            if (prompt != null)
-            {
-                //1초간 메시지를 띄운 다음에 다시 진행
-                Console.Clear();
-                ConsoleUtility.ShowTitle(prompt);
-                Thread.Sleep(1000);//몇 밀리초동안 멈출것인가?
-            }
-
-            Console.Clear();
-
-            ConsoleUtility.ShowTitle("■ 상점 - 구매하기■");
-            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
-            Console.WriteLine("");
-            Console.WriteLine("[보유 골드]");
-            ConsoleUtility.PrintTextHighlights("", player.Gold.ToString(), "G");
-            Console.WriteLine("");
-            Console.WriteLine("[아이템 목록]");
-            for (int i = 0; i < storeInventory.Count; i++)
-            {
-                storeInventory[i].PrintStoreItemStatDesciption(true, i + 1);
-            }
-            Console.WriteLine("");
-            Console.WriteLine("0. 나가기");
-            Console.WriteLine("");
-
-            int keyInput = ConsoleUtility.PromptMenuChoice(0, storeInventory.Count);
-
-            switch (keyInput)
-            {
-                case 0:
-                    StoreMenu();
-                    break;
-                default:
-                    //1 : 이미 구매한 경우
-                    if (storeInventory[keyInput - 1].IsPurchased)
-                    {
-                        PurchaseMenu("이미 구매한 아이템입니다.");
-                    }
-                    //2 : 돈이 충분해서 살 수 있는 경우
-                    else if (player.Gold >= storeInventory[keyInput - 1].Price)
-                    {
-                        player.Gold -= storeInventory[keyInput - 1].Price;
-                        storeInventory[keyInput - 1].Purchased();
-                        inventory.Add(storeInventory[keyInput - 1]);
-                        PurchaseMenu();
-                    }
-                    //3 : 돈이 모자라는 경우
-                    else
-                    {
-                        PurchaseMenu("Gold가 부족합니다.");
-                    }
-                    break;
-
-            }
-        }
+        
 =======
 >>>>>>> Stashed changes
     }
