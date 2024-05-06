1448610e737b8ac05e035288b4b0adf0b73e3d74
CHICHI <smartb12@naver.com>
2024-05-06 14:53:30 +0900

    인터페이스 스탯 수정

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
@@ -11,12 +11,14 @@ namespace TeamSpartaDungeonGame.PlayerInfo
     {
         Player player;
 
-        string Name { get; set; }    // 사용자 이름
+        public string Name { get; set; }    // 사용자 이름
         public int Lv { get; set; }  // 레  벨
         public int Atk { get; set; } // 공격력
         public int Def { get; set; } // 방어력
         public int Hp { get; set; }  // 체  력
+        public int MaxHp { get; set; } // 최대 체력
         public int Mp { get; set; }  // 마  나
+        public int MaxMp {  get; set; } // 최대마나
         public int Gold { get; set; } // 보유 골드
         public int Crit { get; set; } // 크리티컬 확률
         public int Critd { get; set; }// 크리티컬 데미지
@@ -30,7 +32,9 @@ namespace TeamSpartaDungeonGame.PlayerInfo
             Atk = 10;
             Def = 10;
             Hp = 100;
+            MaxHp = 100;
             Mp = 50;
+            MaxMp = 50;
             Crit = 10;
             Critd = 160;
             Dodge = 10;
