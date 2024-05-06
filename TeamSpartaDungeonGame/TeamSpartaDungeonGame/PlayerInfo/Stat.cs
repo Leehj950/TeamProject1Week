8df70eed0c84430be383b35abb258bcabfb2ce20
youminkim <kym1787@naver.com>
2024-05-07 00:41:22 +0900

    스테이터스 수정

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/PlayerInfo/Stat.cs
@@ -11,26 +11,27 @@ namespace TeamSpartaDungeonGame.PlayerInfo
 {
     public class Stat
     {
-        Player player;
-        SceneManager sceneManager;
 
-        public string Name { get; set; }    // 사용자 이름
+        private string name;
+        public string Name { get { return name; } set { name = value; } }    // 사용자 이름
+
+        public string Job { get; set; }
         public int Lv { get; set; }  // 레  벨
         public int Atk { get; set; } // 공격력
         public int Def { get; set; } // 방어력
         public int Hp { get; set; }  // 체  력
         public int MaxHp { get; set; } // 최대 체력
         public int Mp { get; set; }  // 마  나
-        public int MaxMp {  get; set; } // 최대마나
+        public int MaxMp { get; set; } // 최대마나
         public int Gold { get; set; } // 보유 골드
         public int Crit { get; set; } // 크리티컬 확률
         public int Critd { get; set; }// 크리티컬 데미지
         public int Dodge { get; set; }// 회피 확률
-        public int Exp {  get; set; } // 경험치
-        public Stat() 
-
+        public int Exp { get; set; } // 경험치
+        public Stat()
         {
-            Name = player.name;
+            Name = " ";
+            Job = "";
             Lv = 1;
             Atk = 10;
             Def = 10;
@@ -47,9 +48,9 @@ namespace TeamSpartaDungeonGame.PlayerInfo
 
         public void PlayerStatus()
         {
-
             Console.WriteLine("     [스탯 창]     \n");
             Console.WriteLine($"이  름         : {Name}");
+            Console.WriteLine($"직  업         : {Job}");
             Console.WriteLine($"LEVEL          : {Lv}");
             Console.WriteLine($"공격력         : {Atk}");
             Console.WriteLine($"방어력         : {Def}");
@@ -60,17 +61,7 @@ namespace TeamSpartaDungeonGame.PlayerInfo
             Console.WriteLine($"회피 확률 : {Dodge}\n");
             Console.WriteLine($"보유 골드 : {Gold}\n");
             Console.WriteLine($"현재 경험치 : {Exp}\n");
-
-            Console.WriteLine("0. 나가기\n");
-            Console.Write("원하는 행동을 선택해주세요\n>> ");
-            int input = ConsoleUtility.PromptMenuChoice(0, 0);
-
-            if (input == 0)
-            {
-                sceneManager.SceneGameLobby();
-            }
         }
-
     }
 }
 
