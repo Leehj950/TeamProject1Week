484cf36bcae26c5081594c4f2f7c78ded6fe2de9
CHICHI <smartb12@naver.com>
2024-05-06 17:52:22 +0900

    EnemyStats

--- TeamSpartaDungeonGame/TeamSpartaDungeonGame/EnemyInfo/EnemyStats.cs
+++ TeamSpartaDungeonGame/TeamSpartaDungeonGame/EnemyInfo/EnemyStats.cs
@@ -10,6 +10,7 @@ namespace TeamSpartaDungeonGame.EnemyInfo
     {
         public string Name { get; set; }
         public int Level { get; set; }
+        public int Hp {  get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
         public int Gold { get; set; }
@@ -17,10 +18,11 @@ namespace TeamSpartaDungeonGame.EnemyInfo
 
         public bool Islive {  get; set; }
 
-        public EnemyStats(string name, int level, int  atk, int def, int gold, int exp)
+        public EnemyStats(string name, int level,int hp, int  atk, int def, int gold, int exp)
         {
             Name = name;
             Level = level;
+            Hp = hp;
             Atk = atk;
             Def = def;
             Gold = gold;
@@ -31,9 +33,33 @@ namespace TeamSpartaDungeonGame.EnemyInfo
         {
             Name = "미니언";
             Level = 2;
+            Hp = 15;
             Atk = 5;
-            Def = 10;
+            Def = 5;
+            Gold = 100;
+            Exp = 10;
+        }
 
+        public void VoidBug(EnemyStats stats)
+        {
+            Name = "공허충";
+            Level = 3;
+            Hp = 10;
+            Atk = 9;
+            Def = 5;
+            Gold = 300;
+            Exp = 20;
+        }
+
+        public void CanonMinion(EnemyStats stats)
+        {
+            Name = "대포미니언";
+            Level = 5;
+            Hp = 25;
+            Atk = 8;
+            Def = 5;
+            Gold = 500;
+            Exp = 30;
 
         }
     }
